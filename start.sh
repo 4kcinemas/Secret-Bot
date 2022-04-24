9fe506957c3674e922226d30af37cb8e93b0fafd
Rahul Rahaman <rahulrahaman484@gmail.com>
2022-04-24 15:35:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/rahulrahamanx/Netflix-AutoFilterBot-.git
+  git clone https://github.com/rahulrahamanx/Netflix-AutoFilterBot-.git \Netflix-AutoFilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Netflix-AutoFilterBot-
+  git clone $UPSTREAM_REPO /Netflix-AutoFilterBot
 fi
-cd /Netflix-AutoFilterBot-
+cd /Netflix-AutoFilterBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
