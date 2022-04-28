f304401caa9560862867ff2de85712f0aed09e3e
Rahul Rahaman <rahulrahaman484@gmail.com>
2022-04-28 17:48:22 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -105,7 +105,9 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('🔍 Search Again', switch_inline_query_current_chat=" ")
+            InlineKeyboardButton('🔍 Search Here', switch_inline_query_current_chat=" ")
+        ],[
+            InlineKeyboardButton('🧿 Go Inline', switch_inline_query=" ")
         ]
         ]
     return InlineKeyboardMarkup(buttons)
