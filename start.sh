008d4666ace526bfdd28ad1606d82ede991cc8b5
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-05-04 21:54:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/4kcinemas/SeSecret-Bot.git /Secret-Bot
+  git clone https://github.com/4kcinemas/Secret-Bot.git /Secret-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Secret-Bot
